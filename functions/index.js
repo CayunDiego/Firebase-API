8b130bac669d6beca4f8eb140777978624cf4061
DiegoCayun <peridiegoc@hotmail.com>
2020-09-03 04:14:41 -0300

    [ADD] routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert('./permissions.json'),
+    databaseURL: 'https://fir-api-1b408.firebaseio.com'
+});
+
+
+app.get('/hello-word', (req, res) => {
+    return res.status(200).json({
+        message: 'Hello word'
+    });
+});
+
+app.use(require('./routes/products.routes'));
+
+//pasamos nuestro backend dentro de firebase para que pueda ser ejecutado.
+exports.app = functions.https.onRequest(app);
